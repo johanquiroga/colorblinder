13565e394fc266c8c8ac88efbbe8231824169187
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-08-10 19:33:17 -0500

    feat: project scaffold. Initial layout and navigation set-up

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+import Routes from './screens/Routes';
+
+class App extends Component {
+  state = {
+    isFontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      dogbyte: require('./assets/fonts/dogbyte.otf'),
+    });
+    this.setState({ isFontLoaded: true });
+  }
+
+  render() {
+    if (!this.state.isFontLoaded) {
+      return <AppLoading />;
+    }
+    return <Routes />;
+  }
+}
+
+export default App;
