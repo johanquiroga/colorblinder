6d26638473bc15cd496e3597967404dd676a6c75
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-08-11 19:31:47 -0500

    styles: fixed layout of Game screen and added some bottom bar data

--- screens/Game/Game.js
+++ screens/Game/Game.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Dimensions, TouchableOpacity, Text, Image } from 'react-native';
 
 import { Header } from '../../components';
 
 import styles from './styles';
 
 import { generateRGB, mutateRGB } from '../../utils';
+
+import bestPointsIcon from '../../assets/icons/trophy.png';
+import bestTimeIcon from '../../assets/icons/clock.png';
 
 class Game extends React.Component {
   state = {
@@ -62,44 +65,76 @@ class Game extends React.Component {
   };
 
   render() {
-    const { rgb, size, diffTileIndex, diffTileColor } = this.state;
+    const {
+      rgb,
+      size,
+      diffTileIndex,
+      diffTileColor,
+      points,
+      timeLeft,
+    } = this.state;
     const { width } = Dimensions.get('window');
     return (
       <View style={styles.container}>
-        <Header />
-        <View
-          style={{
-            height: width * 0.875,
-            width: width * 0.875,
-            flexDirection: 'row',
-          }}
-        >
-          {Array(size)
-            .fill()
-            .map((_, columnIndex) => (
-              <View
-                style={{ flex: 1, flexDirection: 'column' }}
-                key={columnIndex}
-              >
-                {Array(size)
-                  .fill()
-                  .map((__, rowIndex) => (
-                    <TouchableOpacity
-                      key={`${rowIndex}.${columnIndex}`}
-                      style={{
-                        flex: 1,
-                        backgroundColor:
-                          rowIndex === diffTileIndex[0] &&
-                          columnIndex === diffTileIndex[1]
-                            ? diffTileColor
-                            : `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`,
-                        margin: 2,
-                      }}
-                      onPress={() => this.onTilePress(rowIndex, columnIndex)}
-                    ></TouchableOpacity>
-                  ))}
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Header />
+        </View>
+        <View style={{ flex: 5, justifyContent: 'center' }}>
+          <View
+            style={{
+              height: width * 0.875,
+              width: width * 0.875,
+              flexDirection: 'row',
+            }}
+          >
+            {Array(size)
+              .fill()
+              .map((_, columnIndex) => (
+                <View
+                  style={{ flex: 1, flexDirection: 'column' }}
+                  key={columnIndex}
+                >
+                  {Array(size)
+                    .fill()
+                    .map((__, rowIndex) => (
+                      <TouchableOpacity
+                        key={`${rowIndex}.${columnIndex}`}
+                        style={{
+                          flex: 1,
+                          backgroundColor:
+                            rowIndex === diffTileIndex[0] &&
+                            columnIndex === diffTileIndex[1]
+                              ? diffTileColor
+                              : `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`,
+                          margin: 2,
+                        }}
+                        onPress={() => this.onTilePress(rowIndex, columnIndex)}
+                      ></TouchableOpacity>
+                    ))}
+                </View>
+              ))}
+          </View>
+        </View>
+        <View style={{ flex: 2 }}>
+          <View style={styles.bottomContainer}>
+            <View style={styles.bottomSectionContainer}>
+              <Text style={styles.counterCount}>{points}</Text>
+              <Text style={styles.counterLabel}>points</Text>
+              <View style={styles.bestContainer}>
+                <Image source={bestPointsIcon} style={styles.bestIcon}></Image>
+                <Text style={styles.bestLabel}>0</Text>
               </View>
-            ))}
+            </View>
+            <View style={styles.bottomSectionContainer}></View>
+            <View style={styles.bottomSectionContainer}>
+              <Text style={styles.counterCount}>{timeLeft}</Text>
+              <Text style={styles.counterLabel}>seconds left</Text>
+              <View styles={styles.bestContainer}>
+                <Image source={bestTimeIcon} style={styles.bestIcon}></Image>
+                <Text style={styles.bestLabel}>0</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </View>
     );
